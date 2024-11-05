a4b994fd99b3154293193f9aac419f990f2e1c7e
Dmitry Maslov <dimaslov@5th.ru>
2024-11-05 14:23:48 +0700

    Добавил кнопку скопировать код

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
-navigator.clipboard.writeText("текст для копирования")
-  .then(() => console.log("Скопировано!"))
-  .catch(() => console.log("Скопировать не удалось"));
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+}
+
+function loaded() {
+  document.querySelectorAll(".btn__copy").forEach((btn) => {
+    btn.addEventListener('click', () => {
+      copyText(btn.nextElementSibling.textContent);
+    })
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loaded);
